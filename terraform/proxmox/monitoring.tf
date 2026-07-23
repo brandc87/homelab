56c2f3ca4bc5f4789f69b1a767a397ed0436bb40
Carl Brand <11300674+brandc87@users.noreply.github.com>
2026-07-23 10:50:34 +0100

    Add pre-commit hooks and yamllint config; terraform fmt fixes
    Adds gitleaks, terraform_fmt/tflint, yamllint, and general hygiene
    hooks to catch formatting and secret issues before commit.

--- terraform/proxmox/monitoring.tf
+++ terraform/proxmox/monitoring.tf
@@ -1,6 +1,6 @@
 resource "proxmox_virtual_environment_container" "monitoring" {
-  node_name = "pve"
-  vm_id = 303
+  node_name    = "pve"
+  vm_id        = 303
   unprivileged = true
 
   initialization {
@@ -14,14 +14,14 @@ resource "proxmox_virtual_environment_container" "monitoring" {
     }
 
     user_account {
-      keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEV6AjjRiEIjzcwvE1j7KZixRsNO1fvEHdwEaVvze99i"]
+      keys     = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEV6AjjRiEIjzcwvE1j7KZixRsNO1fvEHdwEaVvze99i"]
       password = null
     }
   }
 
   operating_system {
     template_file_id = "local:vztmpl/debian-12-standard_12.12-1_amd64.tar.zst"
-    type = "debian"
+    type             = "debian"
   }
 
   cpu {
@@ -34,12 +34,12 @@ resource "proxmox_virtual_environment_container" "monitoring" {
 
   disk {
     datastore_id = "local-lvm"
-    size = 16
+    size         = 16
   }
 
   network_interface {
-    name = "eth0"
-    bridge = "vmbr0"
+    name    = "eth0"
+    bridge  = "vmbr0"
     vlan_id = 30
   }
 
--- terraform/proxmox/provider.tf
+++ terraform/proxmox/provider.tf
@@ -1,14 +1,16 @@
 terraform {
+  required_version = ">= 1.8.0"
+
   required_providers {
     proxmox = {
-      source = "bpg/proxmox"
+      source  = "bpg/proxmox"
       version = "~> 0.111"
     }
   }
 }
 
 provider "proxmox" {
-  endpoint = "https://10.15.1.10:8006/"
+  endpoint  = "https://10.15.1.10:8006/"
   api_token = "terraform@pve!tf=${var.pve_token_secret}"
-  insecure = true
+  insecure  = true
 }
